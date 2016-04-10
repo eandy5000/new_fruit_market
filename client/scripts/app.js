86fb861de641e0f2739dd66eff715d268c353c6c
E. Andrew Eccleston <eandreweccleston@Es-MacBook-Pro.local>
2016-04-10 08:05:43 -0500

    added new branch

---
+++ client/scripts/app.js
@@ -0,0 +1,103 @@
+const startPrice = 5;
+const startMoney = 100;       
+
+
+// functions        
+function priceChange () {
+    const min = 1;
+    const max = 100;
+    
+  var num = Math.floor(Math.random()*(1 + max - min)+1);
+    if(num >= 50) {
+        num = num - 50;
+        num = num / 100;
+        return parseFloat(num.toFixed(2));
+    }
+    if(num < 50) {
+        return num / (-100);
+    }
+}       
+       
+// fruit object       
+function Fruit (name, price, image){
+        this.name = name;
+        this.price = price;
+        this.image = image;
+}
+       
+Fruit.prototype = {
+    get fruitPrice () {
+        return this.price;
+    },
+    set fruitPrice (num) {
+        return this.price = this.price + num;
+    },
+    set image (url) {
+        return this.image = url;
+    },
+    get image () {
+        return this.image;
+    }
+}
+       
+// player object
+function Player (name, money) {
+    this.name = name;
+    this.money = money;
+    this.inventory = [];
+}
+
+Player.prototype = {
+    get playerMoney () {
+        return this.money;
+    },
+    set playerMoney (num) {
+        return this.money = this.money + num;
+    },
+    get playerInventory () {
+        return this.inventory;
+    },
+    
+    set playerInventory (item) {
+        return this.inventory.push(item)
+    }
+}  
+
+// purchase object
+
+function Purchase (name, price) {
+    this.price = price;
+    this.name = name;
+}
+       
+Purchase.prototype = {
+    get purchaseName () {
+        return this.name;
+    },
+    get purchasePrice () {
+        return this.price;
+    },
+    set purchasePrice (num) {
+        return this.price = this.price + num;
+    }
+}       
+       
+// game setup                 
+       
+       var joe = new Player("Joe", startMoney);
+       
+       var apple = new Purchase("Apple",5);
+       
+// game mechanics        
+       function countDown () {
+           var num = 10;
+           setInterval(function(){
+               if(num < 1) return;
+               console.log(num);
+               num--;
+           },1000);
+           
+       }
+       
+       countDown();      
+       
---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin');
+    
+    
+gulp.task('default',['index','script','comboSass','scss','watch']);
+
+
+gulp.task('watch', function(){
+   gulp.watch('client/views/index.html',['index']); 
+   gulp.watch('client/scripts/*.js',['script']);
+   gulp.watch('client/styles/scss/*.scss',['comboSass','scss']);
+});
+
+gulp.task('index', function(){
+   return gulp.src('client/views/index.html')
+            .pipe(gulp.dest('public/views/')); 
+});
+
+gulp.task('script', function(){
+   return gulp.src('client/scripts/*.js')
+            .pipe(uglify())
+            .pipe(gulp.dest('public/scripts/')); 
+});
+
+gulp.task('comboSass', function(){
+   return gulp.src('client/styles/scss/*.scss')
+          .pipe(concat('style.scss'))
+          .pipe(gulp.dest('client/styles/'));
+});
+
+gulp.task('scss', function(){
+   return sass('client/styles/style.scss')
+             .pipe(gulp.dest('public/styles/')); 
+});
+
+gulp.task('image', function (){
+   return gulp.src('client/views/images/*')
+            .pipe(imagemin())
+            .pipe(gulp.dest('public/views/images'));
+});
